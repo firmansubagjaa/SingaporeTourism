dda50ebbce0823295fd36349385c527603c246e2
firmanssssss <avenged.firman33@gmail.com>
2023-04-05 03:23:32 +0700

    feat: add data location

--- src/components/maps/index.jsx
+++ src/components/maps/index.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { GoogleMap, MarkerF, useLoadScript } from "@react-google-maps/api";
+import dataLocation from "../../constants/locations.json";
 
 export default function MapsSection() {
+  const [datas, setDatas] = useState([]);
+  const dataToString = JSON.stringify(dataLocation);
+  const parseData = JSON.parse(dataToString);
+  const { location } = parseData;
+  console.log(location);
+  // setDatas(parseData);
+
+  // console.log(data);
+
+  // const locations = useState();
   // create API from google console
   // source ENV problem in Vite ReactJS = https://stackoverflow.com/questions/70709987/how-to-load-environment-variables-from-env-file-using-vite
   const { isLoaded } = useLoadScript({ googleMapsApiKey: import.meta.env.VITE_REACT_GOOGLE_MAP_API_KEY });
@@ -13,8 +24,14 @@ export default function MapsSection() {
     return (
       // set GoogleMap and MarkerF
       <GoogleMap zoom={15} center={{ lat: 1.28692, lng: 103.85457 }} mapContainerClassName="w-full h-full">
-        <MarkerF zoom={17} position={{ lat: 1.28692, lng: 103.85457 }} />
-        <MarkerF zoom={17} position={{ lat: 1.287466, lng: 103.851424 }} />
+        {location.map((items) => {
+          return (
+            <>
+              <MarkerF zoom={17} position={{ lat: items.Latitude, lng: items.Longitude }} />
+            </>
+          );
+        })}
+        {/* <MarkerF zoom={17} position={{ lat: 1.287466, lng: 103.851424 }} /> */}
       </GoogleMap>
     );
   };
